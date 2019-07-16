e26e2e81d178e4e3c15c9864dadbad1ae6891766
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-16 17:35:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ class Route {
     this.endingLocation   = endingLocation;
   }
   blocksTravelled () {
+        let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
     return (this.endingLocation.vertical  - this.beginingLocation.vertical) + eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginingLocation.horizontal);
   }
 }

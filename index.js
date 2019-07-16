ce22b6581aebce33123c46770d839ee28cff8a55
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-16 17:32:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ class Route {
     this.endingLocation   = endingLocation;
   }
   blocksTravelled () {
-    return (this.endingLocation.vertical  - this.beginingLocation.vertical) + eastWest.indexOf(this.endingLocation.horizontal - this.beginingLocation.horizontal);
+    return (this.endingLocation.vertical  - this.beginingLocation.vertical) + eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginingLocation.horizontal);
   }
 }

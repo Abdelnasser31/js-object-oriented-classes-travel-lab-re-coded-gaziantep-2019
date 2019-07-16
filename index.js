d2e42e3c8d6dae7b7b242ccde2845fd5555f6a06
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-16 17:19:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 class Driver {
   constructor (name,startDate) {
     this.name      = name;
-    this.startDate      = new Date(startDate); // there is 
+    this.startDate      = new Date(startDate); 
   }
   yearsExperienceFromBeginningOf(year) {
     let dt = year - this.startDate.getFullYear();
